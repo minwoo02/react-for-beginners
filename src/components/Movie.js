d44b3c2fa97b42167c22bb8ae3154ce73b269a6f
minwoo02 <onyx9250@naver.com>
2023-09-10 22:23:54 +0900

    Update Styles

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,15 +1,17 @@
 import PropsTypes from "prop-types";
 import { Link } from "react-router-dom";
+import styles from "./Movie.module.css";
 
-function Movie({ id, coverImg, title, summary, genres }) {
+function Movie({ id, coverImg, title, year, summary, genres }) {
   return (
-    <div>
-      <img src={coverImg} alt={title} />
-      <h2>
+    <div className={styles.movie}>
+      <img src={coverImg} alt={title} className={styles.movie__img} />
+      <h2 className={styles.movie__title}>
         <Link to={`/movie/${id}`}>{title}</Link>
       </h2>
+      <h3 className={styles.movie__year}>{year}</h3>
       <p>{summary}</p>
-      <ul>
+      <ul className={styles.movie__genres}>
         <li>
           {genres.map((g) => (
             <li key={g}>{g}</li>
